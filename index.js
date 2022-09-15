7f9381ec22492919203f56c51fd9cfaa77515d87
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-09-14 19:49:10 -0700

    changes

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
     document.body.append(image)
     return image
 }
 
-function move(){
-    image.style.position = 'fixed'
-    
-
-    function moveToCoordinates(left, bottom,){
-        image.style.left = left + 'px'
-        image.style.bottom = bottom + 'px'
-    }
-
-    return (
-        to: moveToCoordinates
-    )
-}
-
-let greenCharacter = newImage('assets/green-character.gif')
-move(greenCharacter) to(100,250)
-
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function newItem(url){
+    let item = newImage(url)
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
@@ -46,9 +22,6 @@ newItem('assets/staff.png', 600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
     inventory.style.width = '100%'
     inventory.style.height = '100px'
     inventory.style.display = 'flex'
@@ -61,4 +34,29 @@ function newInventory(){
     return inventory
 }
 
+function move(element){
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom){
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+
 const inventory = newInventory()
+
+move(inventory).to(0, 0)
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
+
